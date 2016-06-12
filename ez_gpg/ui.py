a364a354e3f2981204d73de5d73336b9c6cd330f
Srdjan Grubor <sgnn7@sgnn7.org>
2016-06-12 16:28:03 -0500

    Added a basic window
    Empty for now but it's enough to have a start at this.

---
+++ ez_gpg/ui.py
@@ -0,0 +1,12 @@
+import gi
+
+gi.require_version('Gtk', '3.0')
+
+from gi.repository import Gtk
+
+class EzGpg:
+    def launch(self):
+        win = Gtk.Window()
+        win.connect("delete-event", Gtk.main_quit)
+        win.show_all()
+        Gtk.main()
--- ui/main_window.py
+++
@@ -1,4 +0,0 @@
-class EzGpg:
-    def launch(self):
-        print("Launching...")
-        pass
