2ad13269a1a129e9ee15471bae7a3d35565ade9d
Srdjan Grubor <sgnn7@sgnn7.org>
2016-06-12 16:40:42 -0500

    Added test combobox to window
    This will be used to pull the real keys at a later point.

--- ez_gpg/ui.py
+++ ez_gpg/ui.py
@@ -7,8 +7,21 @@ from gi.repository import Gtk
 class MainWindow(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title="EZ GPG")
-
         self.connect("delete-event", Gtk.main_quit)
+
+        self.set_border_width(30)
+
+        gpg_keys_list = Gtk.ListStore(str, str)
+        gpg_keys_list.append(["abc", "Key1"])
+        gpg_keys_list.append(["abc", "Key1"])
+        gpg_keys_list.append(["abc", "Key1"])
+        gpg_keys_list.append(["abc", "Key1"])
+
+        gpg_key_combo_box = Gtk.ComboBox.new_with_model_and_entry(gpg_keys_list)
+        gpg_key_combo_box.set_entry_text_column(1)
+
+        self.add(gpg_key_combo_box)
+
 
 class EzGpg(Gtk.Window):
     def launch(self):
