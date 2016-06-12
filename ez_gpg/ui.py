1493e8ab22eb8a310aaab0c422392ee1bb96a4cf
Srdjan Grubor <sgnn7@sgnn7.org>
2016-06-12 16:34:48 -0500

    Moved the main window into a separate class
    We don't want all the code to live in the same area so we might as well
    start the split early.

--- ez_gpg/ui.py
+++ ez_gpg/ui.py
@@ -4,13 +4,14 @@ gi.require_version('Gtk', '3.0')
 
 from gi.repository import Gtk
 
-class EzGpg:
+class MainWindow(Gtk.Window):
+    def __init__(self):
+        Gtk.Window.__init__(self, title="EZ GPG")
+
+        self.connect("delete-event", Gtk.main_quit)
+
+class EzGpg(Gtk.Window):
     def launch(self):
-        window = Gtk.Window()
-
-        window.connect("delete-event", Gtk.main_quit)
-        window.set_title("EZ GPG")
-
-        window.show_all()
+        MainWindow().show_all()
 
         Gtk.main()
