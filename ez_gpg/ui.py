d92f3ca36ac30e1a1928ca66c05d504b2add8318
Srdjan Grubor <sgnn7@sgnn7.org>
2016-06-12 16:29:56 -0500

    Added a title to the main window
    Do'h - this is like the most important thing *ever* :)

--- ez_gpg/ui.py
+++ ez_gpg/ui.py
@@ -6,7 +6,11 @@ from gi.repository import Gtk
 
 class EzGpg:
     def launch(self):
-        win = Gtk.Window()
-        win.connect("delete-event", Gtk.main_quit)
-        win.show_all()
+        window = Gtk.Window()
+
+        window.connect("delete-event", Gtk.main_quit)
+        window.set_title("EZ GPG")
+
+        window.show_all()
+
         Gtk.main()
