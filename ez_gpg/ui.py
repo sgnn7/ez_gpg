363b9c2e1baeead8a227c1a21e5b11e8b1f245d8
Srdjan Grubor <sgnn7@sgnn7.org>
2016-06-15 22:23:46 -0500

    Made the app able to encrypt
    This should add the basic functionality to encrypt a file using any
    number of keys though it doesn't add signing to encryptd files yet.

--- ez_gpg/ui.py
+++ ez_gpg/ui.py
@@ -1,3 +1,5 @@
+# vim:ff=unix ts=4 sw=4 expandtab
+
 import gi
 import os
 
@@ -29,11 +31,34 @@ class GenericWindow(Gtk.Window):
     def __init__(self, app, window_name, title):
         window_title = "EZ GPG - %s" % title
 
+        self._app = app
+
         Gtk.Window.__init__(self, title = window_title, application = app)
 
         self.set_border_width(20)
         self.set_name(window_name)
         self.set_position(Gtk.WindowPosition.CENTER)
+
+        self.connect("delete-event", self._close_window)
+
+        self._mapped_actions = {}
+        for action, callback in self._get_actions():
+            simple_action = Gio.SimpleAction.new(action, None)
+            simple_action.connect('activate', callback)
+            app.add_action(simple_action)
+
+            self._mapped_actions[action] = simple_action
+
+    def _close_window(self, *args, **kwargs):
+        self.destroy()
+
+    def _get_actions(self):
+        return []
+
+    def _show_error_message(self, message):
+        print("ERROR! %s" % message)
+        EzGpgUtils.show_dialog(self,
+                               message)
 
 class MainWindow(GenericWindow):
     def __init__(self, app):
@@ -44,6 +69,42 @@ class MainWindow(GenericWindow):
 
         self.add(builder.get_object('main_window_vbox'))
 
+        self._shown_window = None
+
+    def _get_actions(self):
+        return [ ('show_encrypt_ui', self.show_encrypt_ui),
+                 ('show_decrypt_ui', self.show_decrypt_ui),
+                 ('show_sign_ui',    self.show_sign_ui),
+                 ('show_verify_ui',  self.show_verify_ui),
+                 ('key_management',  self.show_key_management_ui),
+               ]
+
+    def show_encrypt_ui(self, action = None, param = None):
+        print("Clicked Encrypt button")
+        child_window = EncryptWindow(self._app)
+        child_window.set_modal(True)
+        child_window.set_transient_for(self)
+
+        child_window.show_all()
+
+        self._app.add_window(child_window)
+
+    def show_decrypt_ui(self, action = None, param = None):
+        print("Clicked Decrypt button")
+        EzGpgUtils.show_unimplemented_message_box(self)
+
+    def show_sign_ui(self, action = None, param = None):
+        print("Clicked Sign button")
+        EzGpgUtils.show_unimplemented_message_box(self)
+
+    def show_verify_ui(self, action = None, param = None):
+        print("Clicked Verify button")
+        EzGpgUtils.show_unimplemented_message_box(self)
+
+    def show_key_management_ui(self, action = None, param = None):
+        print("Clicked Key Management button")
+        EzGpgUtils.show_unimplemented_message_box(self)
+
 class EncryptWindow(GenericWindow):
     def __init__(self, app):
         super().__init__(app, 'encrypt_window', "Encrypt")
@@ -51,14 +112,49 @@ class EncryptWindow(GenericWindow):
         builder = Gtk.Builder()
         builder.add_from_file('data/encrypt_window.ui')
 
-        key_list_box = builder.get_object('lst_key_selection')
+        self._key_list_box = builder.get_object('lst_key_selection')
+        self._file_chooser = builder.get_object('fc_main')
 
         for key_id, key_name, key_friendly_name in EzGpgUtils.get_gpg_keys():
             key_row = Gtk.CheckButton(key_friendly_name)
-            key_list_box.add(key_row)
+            key_row.set_name(key_id)
+
+            self._key_list_box.add(key_row)
 
         self.add(builder.get_object('encrypt_window_vbox'))
 
+    def _get_actions(self):
+        return [ ('encrypt_window.do_encrypt', self.do_encrypt),
+               ]
+
+    def do_encrypt(self, action = None, param = None):
+        print("Clicked Encrypt Content button")
+
+        # TODO: Make this event driven vs post verification
+        print(" - Checking source file(s)")
+        filename = self._file_chooser.get_filename()
+        if not filename:
+            self._show_error_message("File not selected!")
+            return
+
+        print(" - Checking GPG key selection")
+        selected_keys = []
+        for list_box_row in self._key_list_box.get_children():
+            key_item = list_box_row.get_children()[0]
+            if key_item.get_active():
+                key_id = key_item.get_name()
+
+                print("   - Selected: %s" % key_id)
+                selected_keys.append(key_id)
+
+        # TODO: Make this event driven vs post verification
+        if len(selected_keys) == 0:
+            self._show_error_message("No key selected!")
+            return
+
+        EzGpgUtils.encrypt_file(self, filename, selected_keys)
+
+        self.destroy()
 
 class EzGpg(Gtk.Application):
     def __init__(self, *args, **kwargs):
@@ -75,16 +171,7 @@ class EzGpg(Gtk.Application):
         self._actions = [
             ('about', True, self.on_about),
             ('quit',  True, self.on_quit),
-
-            ('show_encrypt_ui', False, self.show_encrypt_ui),
-            ('encrypt_window.do_encrypt', False, self.do_encrypt),
-
-            ('show_decrypt_ui', False, self.show_decrypt_ui),
-            ('show_sign_ui',    False, self.show_sign_ui),
-            ('show_verify_ui',  False, self.show_verify_ui),
-            ('key_management',  False, self.show_key_management_ui),
         ]
-
 
     def do_startup(self):
         print("Starting up...")
@@ -123,46 +210,6 @@ class EzGpg(Gtk.Application):
 
         self._window.present()
 
-    def show_encrypt_ui(self, action = None, param = None):
-        print("Clicked Encrypt button")
-        self._encrypt_window = EncryptWindow(self)
-
-        # self._encrypt_window.connect("delete-event", delete_window)
-        self._encrypt_window.show_all()
-
-        self.add_window(self._encrypt_window)
-
-    def do_encrypt(self, action = None, param = None):
-        print("Clicked Encrypt Content button")
-        self._show_unimplemented_message_box()
-
-    def show_decrypt_ui(self, action = None, param = None):
-        print("Clicked Decrypt button")
-        self._show_unimplemented_message_box()
-
-    def show_sign_ui(self, action = None, param = None):
-        print("Clicked Sign button")
-        self._show_unimplemented_message_box()
-
-    def show_verify_ui(self, action = None, param = None):
-        print("Clicked Verify button")
-        self._show_unimplemented_message_box()
-
-    def show_key_management_ui(self, action = None, param = None):
-        print("Clicked Key Management button")
-        self._show_unimplemented_message_box()
-
-    def _show_unimplemented_message_box(self):
-        dialog = Gtk.MessageDialog(self._window, 0,
-                                   Gtk.MessageType.WARNING,
-                                   Gtk.ButtonsType.OK,
-                                   "Not Implemented")
-        dialog.format_secondary_text("This functionality is not yet implemented!")
-
-        response = dialog.run()
-
-        dialog.destroy()
-
     def do_command_line(self, command_line):
         # options = command_line.get_options_dict()
 
@@ -176,6 +223,7 @@ class EzGpg(Gtk.Application):
         return 0
 
     def on_about(self, action = None, param = None):
+        print("About button pressed")
         about_dialog = Gtk.AboutDialog(transient_for=self._window, modal=True)
         about_dialog.present()
 
--- ez_gpg/utils.py
+++ ez_gpg/utils.py
@@ -1,4 +1,9 @@
+import gi
 import gnupg  # Requires python3-gnupg
+
+gi.require_version('Gtk', '3.0')
+
+from gi.repository import Gtk
 
 class EzGpgUtils(object):
     @staticmethod
@@ -19,3 +24,35 @@ class EzGpgUtils(object):
 
         return keys
 
+    @staticmethod
+    def encrypt_file(window, filename, key_ids):
+        dest_filename = "%s.gpg" % filename
+        print("Encrypting %s to %s!" % (filename, dest_filename))
+
+        gpg = gnupg.GPG()
+        with open(filename, 'rb') as src_file:
+                status = gpg.encrypt_file(src_file,
+                                          recipients=key_ids,
+                                          output=dest_filename)
+
+        print("Encrypted %s to %s!" % (filename, dest_filename))
+        EzGpgUtils.show_dialog(window,
+                               "Completed!\nFile is at %s" % dest_filename,
+                               message_type = Gtk.MessageType.INFO)
+
+    def show_unimplemented_message_box(window):
+        EzGpgUtils.show_dialog(window,
+                               "This functionality is not yet implemented!",
+                               "Not Implemented")
+
+    @staticmethod
+    def show_dialog(window, message, title = "EzGpG", message_type = Gtk.MessageType.WARNING):
+        dialog = Gtk.MessageDialog(window, 0,
+                                   message_type,
+                                   Gtk.ButtonsType.OK,
+                                   title)
+        dialog.format_secondary_text(message)
+
+        response = dialog.run()
+
+        dialog.destroy()
